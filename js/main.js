c92b89bd32d682d0a89544f1628661602694c42a
Petrecho <46752869+Petrecho@users.noreply.github.com>
2019-09-06 17:24:37 +0300

    meadi 2.0

--- js/main.js
+++ js/main.js
@@ -5,68 +5,58 @@
         navLabelActive: 'navigation__item--active',
         hideClass: 'hideContainer',
         showClass: 'showContainer',
-        age: 'age',
-        sidePannel: 'side-pannel',
-        toggleSide: 'side-pannel__toggle',
-        sidePannelSvg: 'side-pannel__toggle--svg',
-        bodyMask: 'panel-body-mask'
-    }
+        age: 'age'
+    };
 
-    const btns = document.querySelectorAll(`.${DOM.navLabel}`);
-    const sections = document.querySelectorAll(`.${DOM.sectionLabel}`);
+    if (window.innerWidth > 900) {
+        const btns = document.querySelectorAll(`.${DOM.navLabel}`);
+        const sections = document.querySelectorAll(`.${DOM.sectionLabel}`);
 
-    let dataBtns = 0;
-    let dataContainers = 0;
-    let isActive = 0;
+        let dataBtns = 0;
+        let dataContainers = 0;
+        let isActive = 0;
 
-    btns.forEach(element => {
-        element.dataset.goto = dataBtns++;
-        element.onclick = nextContainer;
-    });
+        btns.forEach(element => {
+            element.dataset.goto = dataBtns++;
+            element.onclick = nextContainer;
+        });
 
-    sections.forEach(element => {
-        element.dataset.goto = dataContainers++;
-        element.style.opacity = 0;
-        element.style.display = 'none';
-    });
+        sections.forEach(element => {
+            element.dataset.goto = dataContainers++;
+            element.style.opacity = 0;
+            element.style.display = 'none';
+        });
 
-    sections[isActive].style.opacity = 1;
-    sections[isActive].style.display = 'block';
+        sections[isActive].style.opacity = 1;
+        sections[isActive].style.display = 'block';
 
-    function nextContainer() {
-        const data = parseInt(this.dataset.goto);
+        function nextContainer() {
+            const data = parseInt(this.dataset.goto);
 
-        if (data === isActive) {
-            return 0;
-        }
+            if (data !== isActive) {
+                btns[isActive].classList.toggle(DOM.navLabelActive);
+                btns[data].classList.toggle(DOM.navLabelActive);
 
-        btns[isActive].classList.toggle(DOM.navLabelActive);
-        btns[data].classList.toggle(DOM.navLabelActive);
+                sections[isActive].classList.toggle(DOM.hideClass);
+                sections[data].classList.toggle(DOM.showClass);
 
-        sections[isActive].classList.toggle(DOM.hideClass);
-        sections[data].classList.toggle(DOM.showClass);
+                sections[isActive].style.opacity = 0;
 
-        sections[isActive].style.opacity = 0;
-
-       setTimeout(() => {
-            sections[isActive].style.display = 'none';
                 setTimeout(() => {
-                    isActive = data;
-                    sections[data].style.display = 'block'
-                    setTimeout(() => sections[data].style.opacity = 1, 100);
-                }, 100);
-        }, 500);
+                    sections[isActive].style.display = 'none';
+                    setTimeout(() => {
+                        isActive = data;
+                        sections[data].style.display = 'block'
+                        setTimeout(() => sections[data].style.opacity = 1, 100);
+                    }, 100);
+                }, 500);
+            }
+        }
     }
 
-    document.querySelector(`.${DOM.toggleSide}`).addEventListener('click', () => {
-        document.querySelector(`.${DOM.sidePannel}`).classList.toggle('isActive-pannel');
-        document.querySelector(`.${DOM.sidePannelSvg}`).classList.toggle('isActive-toggle');
-        document.querySelector(`.${DOM.bodyMask}`).classList.toggle('active-mask');
-    });
-    
     // Out age on display UI
     const yearOfBirth = 1996;
     const age = new Date().getFullYear() - yearOfBirth;
-    
-    document.querySelector(`#${DOM.age}`).textContent = age;    
+
+    document.querySelector(`#${DOM.age}`).textContent = age;
 })();
