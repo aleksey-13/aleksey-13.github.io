e88039d385d6996bf9eff60f5a556d4e45b58f57
aleksey-13 <alekseymagadan@gmail.com>
2020-06-27 23:16:54 +0300

    add slider

--- app.js
+++ app.js
@@ -19,32 +19,13 @@ AOS.init({
   anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
 
-$(".slider").slick({
-
-  // normal options...
-  infinite: false,
-
-  // the magic
-  responsive: [{
-
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        infinite: true
-      }
-
-    }, {
-
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true
-      }
-
-    }, {
-
-      breakpoint: 300,
-      settings: "unslick" // destroys slick
-
-    }]
+$(document).ready(function () {
+  $(".slider").slick({
+    adaptiveHeight: true,
+    speed: 300,
+    // autoplay: true,
+    waitForAnimate: false,
+    slidesToShow: 1,
+    // fade: true,
+  });
 });
