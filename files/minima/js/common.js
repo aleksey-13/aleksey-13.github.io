5c6c44f70b1c44df5a43315db283ba78ffc36f04
aleksey-13 <alekseymagadan@gmail.com>
2022-01-09 14:47:33 +0200

    upload musik

--- files/minima/js/common.js
+++
@@ -1,7 +0,0 @@
-var wrapperMenu = document.querySelector('.wrapper-menu');
-var menuSlide = document.querySelector('.slideMenu')
-
-wrapperMenu.addEventListener('click', function(){
-	wrapperMenu.classList.toggle('open');
-	menuSlide.classList.toggle('menuS') ;
-})
